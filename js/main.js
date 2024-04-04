f36bcb9fa522b218728efd5df7999b87dc07aed2
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2024-04-03 22:05:52 -0300

    Acrescentando funções do carrinho

--- js/main.js
+++ js/main.js
@@ -21,8 +21,75 @@ btnCancelar.addEventListener('click', ()=> {
 
 const mais = document.querySelector('.modal__btn-add');
 const menos = document.querySelector('.modal__btn-remove');
-const valor = document.getElementById('quanti');
+let valor = parseFloat(document.getElementById('quanti').innerHTML);
 
 menos.addEventListener('click', ()=> {
-    valor.innerHTML = valor.innerHTML.value - 1;
+    console.log(valor);
+    if (valor == 0) {
+
+    } else {
+        valor = valor -1;
+        document.getElementById('quanti').innerHTML = valor;
+    }
 })
+
+mais.addEventListener('click', ()=> {
+    console.log(valor);
+    valor = valor +1;
+    document.getElementById('quanti').innerHTML = valor;
+})
+
+const modalP = document.querySelector('.modal--pequena');
+const modalM = document.querySelector('.modal--media');
+const modalG = document.querySelector('.modal--grande');
+
+modalP.addEventListener('click', ()=> {
+    console.log(modalP);
+    modalP.classList.add('modal--selected');
+    modalM.classList.remove('modal--selected');
+    modalG.classList.remove('modal--selected');
+})
+
+modalM.addEventListener('click', ()=> {
+    console.log(modalM);
+    modalM.classList.add('modal--selected');
+    modalP.classList.remove('modal--selected');    
+    modalG.classList.remove('modal--selected');
+})
+
+modalG.addEventListener('click', ()=> {
+    console.log(modalG);
+    modalG.classList.add('modal--selected');
+    modalP.classList.remove('modal--selected');
+    modalM.classList.remove('modal--selected');    
+})
+
+const btnAddCarrinho = document.querySelector('#modal__botao-adicionar');
+
+btnAddCarrinho.addEventListener('click', ()=> {
+    //1 FECHAR MODAL
+    //2 FECHAR CARRINHO
+    fecharModal();
+    abrirCarrinho();
+})
+
+function fecharModal() {
+    const modal = document.querySelector('.bkg-modal');
+    modal.classList.remove('bkg-modal--ativo');
+}
+
+function abrirCarrinho() {
+    const carrinho = document.querySelector('.carrinho');
+    carrinho.classList.add('carrinho--visivel');
+}
+
+const btnFecharCarrinho = document.querySelector('.carrinho__fechar');
+
+btnFecharCarrinho.addEventListener('click', ()=> {
+    fecharCarrinho();
+})
+
+function fecharCarrinho() {
+    const carrinho = document.querySelector('.carrinho');
+    carrinho.classList.remove('carrinho--visivel');
+}
